d7f29fd0ce4f7796a50d7cb5a5788276a2e69e7b
naveenmehtaison <naveenmehta2024@outlook.com>
2024-05-10 11:16:12 +0530

    Adding Delete And Edit button

--- src/Components/Expenseshower.js
+++ src/Components/Expenseshower.js
@@ -1,7 +1,11 @@
 import axios from "axios"
-import { useState,useEffect } from "react"
+import { useState,useEffect,useRef } from "react"
 const Expenseshower=()=>{
+    const spentRef = useRef(null);
+    const desRef = useRef(null);
+    const selectorRef = useRef(null);
     const [expense,setexpense]=useState([])
+    const [edit,setedit] = useState(false)
     useEffect(()=>{
         async function fetchdata(){
             try{
@@ -21,21 +25,83 @@ const Expenseshower=()=>{
         }
         fetchdata()
     },[])
-async function handleexpense(e){
+    async function DeleteHandler(ele){
+        // e.preventDefault()
+        console.log(ele)
+        try{
+
+            const res = await axios.delete(`https://authentication-1f2ad-default-rtdb.firebaseio.com/expense/${ele.id}.json`)
+            const res2 = await axios.get('https://authentication-1f2ad-default-rtdb.firebaseio.com/expense.json')
+            const dataArray = Object.entries(res2.data).map(([key, value]) => {
+                return { id: key, ...value };
+            });
+            setexpense(dataArray)
+        
+        }
+        catch{
+
+        }
+    }
+async function handleexpense(e,e2){
+
         e.preventDefault()
+
 
         const obj = {SPENT:e.target.spent.value,Des:e.target.des.value,Selector:e.target.selector.value}
         try{
             const res = await axios.post('https://authentication-1f2ad-default-rtdb.firebaseio.com/expense.json',obj)
-            setexpense([obj,...expense])
+            const res2 = await axios.get('https://authentication-1f2ad-default-rtdb.firebaseio.com/expense.json')
+            const dataArray = Object.entries(res2.data).map(([key, value]) => {
+                return { id: key, ...value };
+            });
+            setexpense(dataArray)            
+            
         }
         catch(err){
             console.log(err)
         }
+        spentRef.current.value= ''
+        desRef.current.value=''
+        selectorRef.current.value=''
         
         console.log(expense)
         
 
+    }
+    async function handleedit(ele){
+
+
+        // e.preventDefault()
+
+        const obj = {SPENT:spentRef.current.value,des:desRef.current.value,selector:selectorRef.current.value}
+        console.log(obj)
+        try{
+            const res = await axios.put(`https://authentication-1f2ad-default-rtdb.firebaseio.com/expense/${ele.id}.json`,obj)
+            const res2 = await axios.get('https://authentication-1f2ad-default-rtdb.firebaseio.com/expense.json')
+            const dataArray = Object.entries(res2.data).map(([key, value]) => {
+                return { id: key, ...value };
+            });
+            setexpense(dataArray)     
+        }
+        catch(err){
+            console.log(err)
+        }
+        setedit(false)
+        
+        console.log(expense)
+        
+
+    }
+    const Editfunc=(ele)=>{
+        setedit(true)
+        spentRef.current.value= ele.SPENT
+        desRef.current.value=ele.Des
+        selectorRef.current.value=ele.Selector
+        // const desValue = desRef.current.value;
+        // const selectorValue = selectorRef.current.value;
+        // spentValue=ele.SPENT
+        // desValue=ele.des
+        // selectorValue=ele.selector
     }
     
     return(
@@ -44,17 +110,18 @@ async function handleexpense(e){
                 <div>
                     <form onSubmit={handleexpense}>
                         <label htmlFor="spent">Money Spent</label>
-                        <input id='spent'></input>
+                        <input id='spent' ref={spentRef}></input>
                         <label htmlFor="des">Description</label>
-                        <input id='des'></input>
-                        <select  id="selector">
+                        <input id='des' ref={desRef}></input>
+                        <select  ref={selectorRef} id="selector">
                             <option value="Food">FOOD</option>
                             <option value="Electricity">ELECTRICITY</option>
                             <option value="Travel">TRAVEL</option>
                             <option value="Rent">RENT</option>
                         </select>
-                        <button type="submit">Add</button>
+                        {!edit && <button type="submit">Add</button>}
                     </form>
+                    
                 </div>        
             
             
@@ -66,6 +133,9 @@ async function handleexpense(e){
                         <h3>{ele.SPENT}</h3>
                         <h3>{ele.Des}</h3>
                         <h3>{ele.Selector}</h3>
+                        <button onClick={()=>{DeleteHandler(ele)}}>Delete</button>
+                        <button onClick={()=>{Editfunc(ele)}}>Edit</button>
+                        {edit && <button onClick={()=>{handleedit(ele)}}>Submit Edit</button>}
                         <hr></hr>
                     </div>)
 
